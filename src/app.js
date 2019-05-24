a11970467a551f37dd6ca22a335c18e33da4d1b8
Farah Student <farah.gus@hotmail.com>
2019-05-24 14:24:49 -0400

    fixed node_env

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NOTE_ENV } = require('./config')
+const { NODE_ENV } = require('./config')
 
 const app = express()
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next){
     let response 
-    if (NODE.ENV === "production"){
+    if (NODE_ENV === "production"){
         response = { error: {message: 'server error'}}
     } else {
         console.log(error)
