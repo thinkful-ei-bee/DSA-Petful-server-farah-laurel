ef57d73131a93ad26d87d76c894bc93fb96dba6d
Farah Student <farah.gus@hotmail.com>
2019-06-25 13:56:29 -0400

    client origin

--- src/app.js
+++ src/app.js
@@ -18,9 +18,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors({
-    origin: config.CLIENT_ORIGIN
-}))
+app.use(cors())
 
 
 app.use(catRouter);
