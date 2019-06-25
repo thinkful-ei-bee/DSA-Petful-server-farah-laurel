4656fff4a78b0b0dc57680ea10e3e7e04982e539
Farah Student <farah.gus@hotmail.com>
2019-06-25 13:31:42 -0400

    required config

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const config = require('./config')
 
 const dogRouter = require('./dog-router');
 const catRouter = require('./cat-router');
@@ -18,7 +19,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors({
-    origin: CLIENT_ORIGIN
+    origin: config.CLIENT_ORIGIN
 }))
 
 
