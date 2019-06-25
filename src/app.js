03a0d3a678ce30aaa98d911797321d76312647d5
Farah Student <farah.gus@hotmail.com>
2019-06-25 14:22:53 -0400

    cors issue

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const config = require('./config')
+const {CLIENT_ORIGIN} = require('./config')
 
 const dogRouter = require('./dog-router');
 const catRouter = require('./cat-router');
@@ -18,7 +18,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+  );
 
 
 app.use(catRouter);
